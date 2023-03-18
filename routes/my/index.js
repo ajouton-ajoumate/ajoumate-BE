5f3309d1d4676279b3e68fb366c1bf0cb1bc74d7
zcae1020 <zcae1020@naver.com>
2023-03-18 12:04:55 +0000

    feat: 내 신청목록 불러오기 api 구현

--- routes/my/index.js
+++ routes/my/index.js
@@ -3,10 +3,31 @@ const { getFirestore } = require("firebase-admin/firestore");
 
 const db = getFirestore();
 const joinsRef = db.collection("join");
+const groupsRef = db.collection("group");
+const userRef = db.collection("user");
 
 router.get("/", async (req, res) => {});
 
-router.get("/join", async (req, res) => {});
+router.get("/join", async (req, res) => {
+  try {
+    const UserID = req.body.UserID;
+
+    const joinList = await joinsRef.doc(UserID).get();
+    let responseBody = {};
+    responseBody.Groups = [];
+
+    joinList.forEach((GroupID) => {
+      const group = groupgroupsRef.doc(GroupID).get();
+      responseBody.Groups.push(group);
+    });
+
+    res.send(responseBody);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+    res.send(err);
+  }
+});
 
 router.get("/apply", async (req, res) => {});
 
