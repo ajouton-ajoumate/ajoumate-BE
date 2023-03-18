e8793427313cd38ebbe249ec4211bbdb2fd06c36
zcae1020 <zcae1020@naver.com>
2023-03-18 08:53:02 +0000

    feat: signup api 구현

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,21 +1,26 @@
 const router = require("express").Router();
 
 const db = getFirestore();
+const userRef = db.collection("user");
 
 router.post("/signup", async (req, res, next) => {
+  try {
+    let body = req.body;
+    const res = await userRef.add(body);
 
+    console.log("Added document with ID: ", res.id);
+    body.userID = res.id;
+
+    res.send(body);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post("/signin", async (req, res, next) => {
+router.post("/signin", async (req, res, next) => {});
 
-});
+router.get("/checkDuplicateID", async (req, res, next) => {});
 
-router.get("/checkDuplicateID", async (req, res, next) => {
-
-});
-
-router.get("/checkDuplicateName", async (req, res, next) => {
-
-});
+router.get("/checkDuplicateName", async (req, res, next) => {});
 
 module.exports = router;
